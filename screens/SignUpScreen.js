3754a5c5b1f980526ae434865684191367d71f9f
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-07 09:14:55 +0100

    Update SignUpScreen.js

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -13,7 +13,7 @@ const SignupScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       {/* Logo */}
-      <Image source={require('../assets/Logo1.png')} style={styles.logo} />
+      <Image source={require('../assets/Logo1.jpg')} style={styles.logo} />
 
       {/* Welcome text */}
       <Text style={styles.welcomeText}>Join PantryHub</Text>
