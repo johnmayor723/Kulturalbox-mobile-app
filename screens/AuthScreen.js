610b7e8cf754dd916500c62661918e449957d290
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-08 07:45:31 +0100

    Update AuthScreen.js

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -58,8 +58,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
   },
   logo: {
-    width: 100,
-    height: 100,
+    width: 200,
+    height: 200,
     marginBottom: 20,
   },
   welcomeText: {
