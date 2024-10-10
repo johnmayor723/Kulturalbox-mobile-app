3d2d7b9e55b68d9a1b8db3d66904a83a1414ec3e
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-10 20:21:49 +0100

    Update OilProductsScreen.js

--- screens/OilProductsScreen.js
+++ screens/OilProductsScreen.js
@@ -37,7 +37,7 @@ export default function CategoriesScreen({ navigation }) {
       {/* Header Section */}
       <View style={styles.headerSection}>
         <Text style={styles.sectionHeader}>Categories</Text>
-        <Text style={styles.sectionHeader}>Oil Products</Text>
+        <Text style={styles.sectionHeader2}>Oil Products</Text>
       </View>
 
       {/* Content Section */}
@@ -118,6 +118,13 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'green',
   },
+   sectionHeader2: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'green',
+    marginRight: 90,
+  },
+
   contentSection: {
     flex: 1,
     flexDirection: 'row',
