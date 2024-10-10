faac461cac7a913cda9bcc7e16c7df993fd1cda9
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-10 18:00:38 +0100

    Update SnacksScreen.js

--- screens/SnacksScreen.js
+++ screens/SnacksScreen.js
@@ -37,7 +37,7 @@ export default function CategoriesScreen({ navigation }) {
       {/* Header Section */}
       <View style={styles.headerSection}>
         <Text style={styles.sectionHeader}>Categories</Text>
-        <Text style={styles.sectionHeader}>Snacks</Text>
+        <Text style={styles.sectionHeader2}>Snacks</Text>
       </View>
 
       {/* Content Section */}
@@ -117,6 +117,12 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     color: 'green',
+  },
+  sectionHeader2: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'green',
+    marginRight: 100,
   },
   contentSection: {
     flex: 1,
