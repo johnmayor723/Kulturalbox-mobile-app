51794166fa779f9dcaac3e8848ad776c815c5457
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-24 22:45:41 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-//App.js
-
 import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Ionicons } from '@expo/vector-icons';
+import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Import Screens
@@ -16,18 +15,18 @@ import HomeScreen from './screens/HomeScreen';
 import UserProfileScreen from './screens/UserProfileScreen';
 import CartScreen from './screens/CartScreen';
 import ContactScreen from './screens/ContactScreen';
-import PaymentScreen from "./screens/PaymentScreen";
-import OrderTrackingScreen from "./screens/OrderTrackingScreen";
-import HelpScreen from "./screens/HelpScreen";
-import TrackingScreen from "./screens/TrackingScreen";
-import ItemScreen from "./screens/ItemScreen.js";
-import CategoriesScreen from "./screens/CategoriesScreen.js";
-import FruitsScreen from "./screens/FruitsScreen.js";
-import VegetablesScreen from "./screens/VegetablesScreen.js";
-import DairyScreen from "./screens/DairyScreen.js";
-import MeatScreen from "./screens/MeatScreen.js";
-import OilProductsScreen from "./screens/OilProductsScreen.js";
-import SnacksScreen from "./screens/SnacksScreen.js";
+import PaymentScreen from './screens/PaymentScreen';
+import OrderTrackingScreen from './screens/OrderTrackingScreen';
+import HelpScreen from './screens/HelpScreen';
+import TrackingScreen from './screens/TrackingScreen';
+import ItemScreen from './screens/ItemScreen.js';
+import CategoriesScreen from './screens/CategoriesScreen.js';
+import FruitsScreen from './screens/FruitsScreen.js';
+import VegetablesScreen from './screens/VegetablesScreen.js';
+import DairyScreen from './screens/DairyScreen.js';
+import MeatScreen from './screens/MeatScreen.js';
+import OilProductsScreen from './screens/OilProductsScreen.js';
+import SnacksScreen from './screens/SnacksScreen.js';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -35,178 +34,121 @@ const Drawer = createDrawerNavigator();
 
 // Bottom Tab Navigator Component
 function BottomTabNavigator() {
-    return (
-        <Tab.Navigator
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ color, size }) => {
-                    let iconName;
-                    if (route.name === 'TabHome') {
-                        iconName = 'home';
-                    } else if (route.name === 'TabCategories') {
-                        iconName = 'grid-outline';
-                    } else if (route.name === 'TabProfile') {
-                        iconName = 'person';
-                    } else if (route.name === 'TabCart') {
-                        iconName = 'cart';
-                    }
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                },
-                tabBarActiveTintColor: '#FF7E00',  // Active icon color
-                tabBarInactiveTintColor: 'gray',   // Inactive icon color
-                tabBarStyle: { backgroundColor: '#fff' },  // Tab bar background color
-                headerShown: false  // Disable the header for the inner tab navigator
-            })}
-        >
-            <Tab.Screen name="TabHome" component={HomeScreen} options={{ title: "Home" }} />
-            <Tab.Screen name="TabCategories" component={CategoriesScreen} options={{ title: "Categories" }} />
-            <Tab.Screen name="TabProfile" component={UserProfileScreen} options={{ title: "Profile" }} />
-            <Tab.Screen name="TabCart" component={CartScreen} options={{ title: "Cart" }} />
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === 'TabHome') {
+            iconName = 'home';
+          } else if (route.name === 'TabCategories') {
+            iconName = 'grid-outline';
+          } else if (route.name === 'TabProfile') {
+            iconName = 'person';
+          } else if (route.name === 'TabCart') {
+            iconName = 'cart';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#FF7E00', // Active icon color
+        tabBarInactiveTintColor: 'gray',  // Inactive icon color
+        tabBarStyle: { backgroundColor: '#fff' },  // Tab bar background color
+        headerShown: false // Disable the header for the inner tab navigator
+      })}
+    >
+      <Tab.Screen name="TabHome" component={HomeScreen} options={{ title: 'Home' }} />
+      <Tab.Screen name="TabCategories" component={CategoriesScreen} options={{ title: 'Categories' }} />
+      <Tab.Screen name="TabProfile" component={UserProfileScreen} options={{ title: 'Profile' }} />
+      <Tab.Screen name="TabCart" component={CartScreen} options={{ title: 'Cart' }} />
+    </Tab.Navigator>
+  );
 }
 
 // Drawer Navigator Component
-function DrawerNavigator() {
-    return (
-        <Drawer.Navigator initialRouteName="DrawerHome">
-            <Drawer.Screen 
-                name="DrawerHome" 
-                component={BottomTabNavigator} 
-                options={{ title: "Menu", headerShown: true }}  
-            />
-            <Drawer.Screen 
-                name="DrawerProfile" 
-                component={UserProfileScreen} 
-                options={{ title: "Profile", headerShown: true }}  
-            />
-            <Drawer.Screen 
-                name="DrawerContact" 
-                component={HelpScreen} 
-                options={{ title: "Help", headerShown: true }}  
-            />
-            <Drawer.Screen 
-                name="DrawerTracking" 
-                component={TrackingScreen} 
-                options={{ title: "Tracking", headerShown: true }}  
-            />
-        </Drawer.Navigator>
-    );
+function DrawerNavigator({ navigation }) {
+  // Logout function
+  const handleLogout = async () => {
+    try {
+      await AsyncStorage.removeItem('token');
+      await AsyncStorage.removeItem('user');
+      Alert.alert('Logged out', 'You have successfully logged out.');
+      navigation.replace('Auth'); // Navigate back to the login screen after logout
+    } catch (error) {
+      console.error('Error during logout:', error);
+    }
+  };
+
+  return (
+    <Drawer.Navigator initialRouteName="DrawerHome">
+      <Drawer.Screen name="DrawerHome" component={BottomTabNavigator} options={{ title: 'Menu', headerShown: true }} />
+      <Drawer.Screen name="DrawerProfile" component={UserProfileScreen} options={{ title: 'Profile', headerShown: true }} />
+      <Drawer.Screen name="DrawerContact" component={HelpScreen} options={{ title: 'Help', headerShown: true }} />
+      <Drawer.Screen name="DrawerTracking" component={TrackingScreen} options={{ title: 'Tracking', headerShown: true }} />
+      <Drawer.Screen name="DrawerLogout" component={null} options={{ title: 'Logout', headerShown: true }} listeners={{
+        focus: () => handleLogout() // Call logout when this drawer item is focused
+      }} />
+    </Drawer.Navigator>
+  );
 }
 
 // Auth Stack Navigator
 function AuthStack() {
-    return (
-        <Stack.Navigator initialRouteName="Splash">
-            <Stack.Screen
-                name="Splash"
-                component={SplashScreen}
-                options={{ headerShown: false }}  // Hides the header
-            />
-            <Stack.Screen
-                name="Auth"
-                component={AuthScreen}
-                options={{ headerShown: false }}  // Hides the header
-            />
-            <Stack.Screen
-                name="SignUp"
-                component={SignUpScreen}
-                options={{ headerShown: false }}  // Hides the header
-            />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Auth" component={AuthScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
 }
 
 // Home Stack Navigator
 function HomeStack() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="Main"
-                component={DrawerNavigator}
-                options={{ headerShown: false }}  // Disable the header for the main drawer stack
-            />
-            <Stack.Screen
-                name="Single Product"
-                component={ItemScreen}
-                options={{ headerShown: true }}
-            />
-            <Stack.Screen
-                name="Cart"
-                component={CartScreen}
-                options={{ headerShown: true }}
-            />
-            <Stack.Screen
-                name="Payment"
-                component={PaymentScreen}
-                options={{ headerShown: true }}
-            />
-            <Stack.Screen
-                name="TrackingResult"
-                component={OrderTrackingScreen}
-                options={{ headerShown: true }}
-            />
-            <Stack.Screen
-                name="Fruit"
-                component={FruitsScreen}
-                options={{ headerShown: true }}
-            />
-            <Stack.Screen
-                name="Vegetable"
-                component={VegetablesScreen}
-                options={{ headerShown: true }}
-            />
-            <Stack.Screen
-                name="Meat"
-                component={MeatScreen}
-                options={{ headerShown: true }}
-            />
-            <Stack.Screen
-                name="Snack"
-                component={SnacksScreen}
-                options={{ headerShown: true }}
-            />
-            <Stack.Screen
-                name="Oil Product"
-                component={OilProductsScreen}
-                options={{ headerShown: true }}
-            />
-            <Stack.Screen
-                name="Dairy"
-                component={DairyScreen}
-                options={{ headerShown: true }}
-            />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Main" component={DrawerNavigator} options={{ headerShown: false }} />
+      <Stack.Screen name="Single Product" component={ItemScreen} options={{ headerShown: true }} />
+      <Stack.Screen name="Cart" component={CartScreen} options={{ headerShown: true }} />
+      <Stack.Screen name="Payment" component={PaymentScreen} options={{ headerShown: true }} />
+      <Stack.Screen name="TrackingResult" component={OrderTrackingScreen} options={{ headerShown: true }} />
+      <Stack.Screen name="Fruit" component={FruitsScreen} options={{ headerShown: true }} />
+      <Stack.Screen name="Vegetable" component={VegetablesScreen} options={{ headerShown: true }} />
+      <Stack.Screen name="Meat" component={MeatScreen} options={{ headerShown: true }} />
+      <Stack.Screen name="Snack" component={SnacksScreen} options={{ headerShown: true }} />
+      <Stack.Screen name="Oil Product" component={OilProductsScreen} options={{ headerShown: true }} />
+      <Stack.Screen name="Dairy" component={DairyScreen} options={{ headerShown: true }} />
+    </Stack.Navigator>
+  );
 }
 
 // Main App Component
 export default function App() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const checkAuthStatus = async () => {
-            try {
-                const user = await AsyncStorage.getItem('user');
-                if (user) {
-                    setIsAuthenticated(true);
-                }
-            } catch (error) {
-                console.error("Error fetching user from AsyncStorage:", error);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-        checkAuthStatus();
-    }, []);
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      try {
+        const user = await AsyncStorage.getItem('user');
+        if (user) {
+          setIsAuthenticated(true);
+        }
+      } catch (error) {
+        console.error('Error fetching user from AsyncStorage:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    checkAuthStatus();
+  }, []);
 
-    if (isLoading) {
-        return <SplashScreen />; // Show a loading screen while fetching auth status
-    }
+  if (isLoading) {
+    return <SplashScreen />; // Show a loading screen while fetching auth status
+  }
 
-    return (
-        <NavigationContainer>
-            {isAuthenticated ? <HomeStack /> : <AuthStack />}
-        </NavigationContainer>
-    );
-                }
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? <HomeStack /> : <AuthStack />}
+    </NavigationContainer>
+  );
+}
