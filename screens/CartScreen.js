84a220bd67efd75c6f000195ea7c09563b4ddd85
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-24 11:31:51 +0100

    Update CartScreen.js

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -27,7 +27,7 @@ const CartScreen = () => {
       <Text>Cart Items</Text>
       <FlatList
         data={cartItems}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item) => item_id.toString()}
         renderItem={({ item }) => (
           <View>
             <Text>{item.name}</Text>
