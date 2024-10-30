20f2bb2c34bd925c8b1d9088c52b12f01ce790b4
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-30 14:38:37 +0100

    Update CheckoutScreen.js

--- screens/CheckoutScreen.js
+++ screens/CheckoutScreen.js
@@ -1,23 +1,27 @@
-import PaystackWebView from 'react-native-paystack-webview'
-import React, { Component } from 'react'
-import { View } from 'react-native'
+import React from 'react';
+import { View } from 'react-native';
+import PaystackWebView from 'react-native-paystack-webview';
 
-class MyApp extends Component {
-  render () {
-    return (
-      <View>
-        <PaystackWebView
-          buttonText='Pay Now'
-          paystackKey='pk_test_bec2adfc8f46afff889349e2bf76e50477939d74'
-          amount={120000}
-          billingEmail='ayoshokz@gmail.com'
-          billingMobile='08101274387'
-          billingName='Oluwatobi Shokunbi'
-          ActivityIndicatorColor='green'
-          onSuccess={(tranRef)=>{console.log(tranRef)}}
-          onCancel={()=>{console.log('something went wrong')}}
-         />
-      </View>
-    )
-  }
-            }
+const CartScreen = () => {
+  return (
+    <View>
+      <PaystackWebView
+        buttonText="Pay Now"
+        paystackKey="pk_test_bec2adfc8f46afff889349e2bf76e50477939d74"
+        amount={120000}
+        billingEmail="ayoshokz@gmail.com"
+        billingMobile="08101274387"
+        billingName="Oluwatobi Shokunbi"
+        ActivityIndicatorColor="green"
+        onSuccess={(tranRef) => {
+          console.log(tranRef);
+        }}
+        onCancel={() => {
+          console.log('Something went wrong');
+        }}
+      />
+    </View>
+  );
+};
+
+export default CartScreen;
