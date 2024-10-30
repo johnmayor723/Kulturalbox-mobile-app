17d9b020f753c8356d6b8583010eda8ca9dac542
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-30 14:42:56 +0100

    Update CheckoutScreen.js

--- screens/CheckoutScreen.js
+++ screens/CheckoutScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import PaystackWebView from 'react-native-paystack-webview';
 
-const ChechoutScreen = () => {
+const CheckoutScreen = () => {
   return (
     <View>
       <PaystackWebView
