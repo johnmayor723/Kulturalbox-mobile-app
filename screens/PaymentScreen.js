fe2fc563ba7e41f0aa1724dd392713a97cdb4fa5
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-11-10 08:41:24 +0100

    Update PaymentScreen.js

--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -17,7 +17,7 @@ const PaymentScreen = () => {
   const formattedTotalAmount = `₦${totalAmount.toFixed(2)}`;
   
   const handleCheckout = () => {
-    navigation.navigate("Checkout", { totalAmount});
+    navigation.navigate("Checkout", { totalAmount:formattedTotalAmount});
   };
 
   return (
