122da3e3d231b0ad7a833d95246ce548c8e5f43c
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-11-17 19:20:50 +0100

    Update ItemScreen.js

--- screens/ItemScreen.js
+++ screens/ItemScreen.js
@@ -13,7 +13,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 
 const addToCart = async (id) => {
-    let itemArray = await AsyncStorage.getItem('cartItem');
+    let itemArray = await AsyncStorage.getItem('cart');
     itemArray = JSON.parse(itemArray) || []; // Initialize as an empty array if null
 
     if (itemArray) {
@@ -24,7 +24,7 @@ const addToCart = async (id) => {
 
         try {
             // Save the updated array back to AsyncStorage
-            await AsyncStorage.setItem('cartItem', JSON.stringify(array));
+            await AsyncStorage.setItem('cart', JSON.stringify(array));
             Alert.alert('Item Added To Cart', JSON.stringify(array, null, 2));
         } catch (error) {
             console.error('Error saving cart items:', error);
@@ -33,7 +33,7 @@ const addToCart = async (id) => {
         let array = [];
         array.push(id);
         try {
-            await AsyncStorage.setItem('cartItem', JSON.stringify(array));
+            await AsyncStorage.setItem('cart', JSON.stringify(array));
             Alert.alert('Item Added To Cart');
         } catch (error) {
             console.error('Error saving cart items:', error);
